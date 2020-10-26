942f6dbe2e3c73b90f671f3c7456bf2cc32c84cb
Baens, Ludovic <ludovic.baens@solidaris.be>
2020-10-26 22:56:22 +0100

    Add Singleton Pattern

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // https://dotnettutorials.net/lesson/singleton-design-pattern/
+            //
+            // Ne pas utiliser en multi-threaded 
+            //
+
+            Singleton developper = Singleton.GetInstance;
+            developper.PrintDetails("From developper");
+
+            Singleton analyst = Singleton.GetInstance;
+            analyst.PrintDetails("From analyst");
+
+            Singleton tester = Singleton.GetInstance;
+            tester.PrintDetails("From tester");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    // sealed : permet que la classe ne puisse pas être héritée
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        private static Singleton instance = null;
+
+        // est utilisée pour renvoyer une seule instance de la classe en vérifiant la valeur de l'instance de variable privée
+        public static Singleton GetInstance
+        {
+            get
+            {
+                // Si l'instance n'a pas été créée, on la créée sinon, on retourne l'instance créée
+                if (instance == null) instance = new Singleton();
+                return instance;
+            }
+        }
+
+        // La classe est créée avec un constructeur privé qui garantira que la 
+        // classe ne sera pas instanciée depuis l'extérieur de la classe
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
